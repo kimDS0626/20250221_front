fd271ab31100ac321fc7b5795306929add2b9893
SIJIN9090 <dol9090@naver.com>
2025-02-21 17:53:57 +0900

    20250221_front-master

--- src/components/user/login/FindId.js
+++ src/components/user/login/FindId.js
@@ -1,128 +1,129 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router";
 import styled from "styled-components";
+import logo_b from "./imgs/logo_b.png";
+function FindId() {
+  return (
+    <FindIdContainer>
+      <FindIdSection>
+        <FindLogo>
+          <img src={logo_b} />
+        </FindLogo>
+
+        <Title>
+          <h4> 아이디찾기 </h4>
+        </Title>
+        <LoginInput>
+          <div>
+            <input type="text" placeholder="이름"></input>
+            <input type="password" placeholder="전화번호"></input>
+          </div>
+        </LoginInput>
+
+        <CheckBox>
+          <button>확인</button>
+        </CheckBox>
+      </FindIdSection>
+    </FindIdContainer>
+  );
+}
 
 const FindIdContainer = styled.div`
-  display: block;
-  height: 1200px;
+  width: 1920px;
+  min-height: 500px;
   width: 100%;
+  max-width: 1920px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 // ----------------------------------------------------------------------------------
 
 const FindIdSection = styled.div`
   margin: auto;
-  justify-content: center;
   align-items: center;
-
-  width: 400px;
-  height: 400px;
+  margin-top: 130px;
+  padding-top: 30px;
+  width: 500px;
+  min-height: 500px;
   background-color: #f4f4f4;
+  margin-bottom: 100px;
 `;
-
-const FindIdBoxA = styled.div`
-  text-align: center;
-  width: 400px;
-  height: 190px;
-  position: relative;
-  bottom: 30px;
+const FindLogo = styled.div`
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  width: 500px;
+  height: 40px;
+  background-color: #f4f4f4;
+  margin-bottom: 30px;
+  img {
+    width: 145px;
+    height: 35px;
+  }
 `;
 
 const Title = styled.div`
-  font-size: 24px;
-  display: block;
-  width: 400px;
-  height: 90px;
-  color: #111111;
-  text-align: center;
-  h1 {
-    padding-top: 30px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  width: 500px;
+  height: 40px;
+  margin-bottom: 30px;
+  h4 {
+    font-size: 36px;
+    color: #111111;
+    font-weight: bold;
   }
 `;
 
 const LoginInput = styled.div`
-  display: block;
-  width: 400px;
-  height: 100px;
-  color: #111111;
+  width: 500px;
+  display: flex;
+  flex-direction: column; /* 세로로 정렬 */
+  align-items: center;
+  margin-bottom: 10px;
+  justify-content: center;
+  background-color: #f4f4f4;
+  gap: 20px; /* input 사이의 간격을 20px로 설정 */
+
+  div {
+    width: 100%;
+    height: 150px;
+  }
+  input:first-child {
+    margin-bottom: 5px;
+  }
   input {
-    color: #111111;
-    background: transparent;
-    width: 250px;
-    padding: 10px;
-    margin-bottom: 10px;
+    outline: none;
+    font-weight: 300;
     border: none;
-    border-bottom: 1px solid #111111;
-    border-radius: 3px;
+    padding-left: 20px;
+    font-size: 20px;
+    width: 460px;
+    height: 60px;
+    display: block;
+    margin: 0 auto;
   }
 `;
-const LoginBoxB = styled.div`
-  text-align: center;
-  display: block;
-  width: 400px;
-  height: 180px;
-  position: relative;
-  bottom: 35px;
-`;
-
-const LoginButton = styled.div`
-  position: relative;
-  top: 20px;
-  float: left;
-  width: 400px;
-  height: 65px;
-
+const CheckBox = styled.div`
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  width: 500px;
+  height: 60px;
+  margin-top: 20px;
+  margin-bottom: 20px;
   button {
-    background: transparent;
-
-    font-size: 26px;
-    width: 400px;
-    height: 65px;
-
-    color: #111111;
+    font-size: 20px;
+    font-weight: 700;
+    width: 460px;
+    height: 60px;
+    background-color: #111111;
+    color: #fff;
   }
 `;
-const SignupButton = styled.div`
-  position: relative;
-  top: 10px;
-  float: left;
-
-  width: 400px;
-  height: 65px;
-  button {
-    background: transparent;
-    border: none;
-
-    font-size: 18px;
-    width: 400px;
-    height: 65px;
-
-    color: #111111;
-  }
-`;
-
-function FindId() {
-  return (
-    <FindIdContainer>
-      <FindIdSection>
-        <FindIdBoxA>
-          <Title>
-            <h4> - 아이디찾기 -</h4>
-          </Title>
-          <LoginInput>
-            <input type="text" placeholder="이름"></input>
-            <input type="password" placeholder="전화번호"></input>
-          </LoginInput>
-        </FindIdBoxA>
-        <LoginBoxB>
-          <LoginButton>
-            <button>확인</button>
-          </LoginButton>
-          <SignupButton></SignupButton>
-        </LoginBoxB>
-      </FindIdSection>
-    </FindIdContainer>
-  );
-}
 
 export default FindId;
